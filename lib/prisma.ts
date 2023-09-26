00556cf074530779320d1640a97f085383aa50f9
Денис Хлобыстов <thisfeelmyfriend@gmail.com>
2023-09-26 22:51:54 +0300

    add vercel-build script

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,10 +1,21 @@
 import { PrismaClient } from "@prisma/client";
 
-
+let prisma:any
 const globalForPrisma = global as unknown as { prisma: PrismaClient }
-const prisma = globalForPrisma.prisma || new PrismaClient()
 
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
+if (process.env.NODE_ENV === 'production') {
+  prisma = new PrismaClient()
+} else {
+  if (!globalForPrisma.prisma) {
+    globalForPrisma.prisma = new PrismaClient()
+  }
+  prisma = globalForPrisma.prisma
+}
+
+// const globalForPrisma = global as unknown as { prisma: PrismaClient }
+// const prisma = globalForPrisma.prisma || new PrismaClient()
+
+// if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
 
 
 export default prisma 
